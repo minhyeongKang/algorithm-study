dc329de71a22bcc2c2cf82c4d8d06c2c5e8d9d26
minhyeongKang <146315365+minhyeongKang@users.noreply.github.com>
2026-04-27 20:01:27 +0900

    [level 0] Title: 코드 처리하기, Time: 348.56 ms, Memory: 392 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        
+        for (int i = 0; i < code.length(); i++) {
+            
+            if (mode == 0 && code.charAt(i) == '1') {
+                mode++;
+            } else if (mode == 1 && code.charAt(i) == '1') {
+                mode--;
+            }
+            
+            if (mode == 0 && code.charAt(i) != '1') {
+                if (i % 2 == 0) {
+                    answer += code.charAt(i);
+                }
+            }
+            
+            if (mode == 1 && code.charAt(i) != '1') {
+                if (i % 2 != 0) {
+                    answer += code.charAt(i);
+                }
+            }
+        }
+        
+        if (answer.equals("")) {
+            answer = "EMPTY";
+        }
+        
+        return answer;
+    }
+}
