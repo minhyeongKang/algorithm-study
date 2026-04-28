90c6cd50e29c67bbd0d90bbb1de660c0283b9d80
minhyeongKang <146315365+minhyeongKang@users.noreply.github.com>
2026-04-28 09:11:18 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        
+        answer = N * (M - 1) + (N - 1);
+        
+        
+        return answer;
+    }
+}
