2b9592511a1feb18971f726712224a65e23d23bb
minhyeongKang <146315365+minhyeongKang@users.noreply.github.com>
2026-04-29 16:11:36 +0900

    [level 1] Title: 예산, Time: 0.55 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        int count = 0;
+        
+        Arrays.sort(d);
+        
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            count++;
+            
+            if (sum <= budget) {
+                answer = count;
+            }
+        }
+        
+        return answer;
+    }
+}
