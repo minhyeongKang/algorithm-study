675650a4efd1f2a5ff2c84cf8cee93b4c8839945
minhyeongKang <146315365+minhyeongKang@users.noreply.github.com>
2026-04-30 10:42:55 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = {};
+        
+        ArrayList<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                for (int j = 0; j < i; j++) {
+                    list.add(str_list[j]);
+                }
+                break;
+            } else if (str_list[i].equals("r")) {
+                for (int j = i+1; j < str_list.length; j++) {
+                    list.add(str_list[j]);
+                }
+                break;
+            }
+        }
+        
+        answer = new String[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
