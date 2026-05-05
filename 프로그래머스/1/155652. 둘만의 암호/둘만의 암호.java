836e3206add3328a2454422e71b173210c87cf25
minhyeongKang <146315365+minhyeongKang@users.noreply.github.com>
2026-05-05 09:52:52 +0900

    [level 1] Title: 둘만의 암호, Time: 13.72 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int count = 0;
+            
+            while (count < index) {
+                ch++;
+                if (ch > 'z') {
+                    ch = 'a';
+                }
+                
+                if (skip.indexOf(ch) == -1) {
+                    count++;
+                }
+            }
+            answer += ch;
+        }
+        
+        return answer;
+    }
+}
